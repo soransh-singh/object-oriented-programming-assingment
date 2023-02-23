ee20103d5d6cfe9ad8451bbde5df06f769349843
soransh <57911107+soransh-singh@users.noreply.github.com>
2023-02-23 16:38:24 +0530

    Initial commit

---
+++ ques01.cpp
@@ -0,0 +1,14 @@
+// Define a Class Largest to obtain the largest of three given numbers.
+
+#include <iostream>
+
+using namespace std;
+
+class Largest{
+    int num1, num2, num3;
+    
+};
+
+int main(){
+    return 0;
+}
